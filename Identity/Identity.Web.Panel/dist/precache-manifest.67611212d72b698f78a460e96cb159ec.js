9d75caa0977fa21ddb0e06d2453bbe649aba30ee
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-12-10 01:24:52 +0330

    Update .gitignore

---
+++ Identity/Identity.Web.Panel/dist/precache-manifest.67611212d72b698f78a460e96cb159ec.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eccc029d997c47160ab",
+    "url": "/css/chunk-0e19f409.aff7ef1b.css"
+  },
+  {
+    "revision": "ca546b79405cadebb107",
+    "url": "/css/chunk-47ccbcc0.59b1b027.css"
+  },
+  {
+    "revision": "08424b5fd76d0d788e00",
+    "url": "/css/chunk-a7133372.3a3b8fda.css"
+  },
+  {
+    "revision": "2b43472b181b040c0830",
+    "url": "/css/chunk-vendors.5eae41e6.css"
+  },
+  {
+    "revision": "7154315a854a780972cea30b3b26b1c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e661c9028b6c4443c64",
+    "url": "/js/app.15283fbc.js"
+  },
+  {
+    "revision": "7eccc029d997c47160ab",
+    "url": "/js/chunk-0e19f409.ca27fb03.js"
+  },
+  {
+    "revision": "c68654232997dae30fe8",
+    "url": "/js/chunk-11069360.08e8a045.js"
+  },
+  {
+    "revision": "ca546b79405cadebb107",
+    "url": "/js/chunk-47ccbcc0.280aa97b.js"
+  },
+  {
+    "revision": "08424b5fd76d0d788e00",
+    "url": "/js/chunk-a7133372.cdf318f7.js"
+  },
+  {
+    "revision": "2b43472b181b040c0830",
+    "url": "/js/chunk-vendors.62d824d4.js"
+  },
+  {
+    "revision": "4a92bfc19f6faae09a49eea958e8fc20",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
