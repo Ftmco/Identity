45f8c403fd959eafb5733df83e9d8764f12ef10a
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-12-10 20:27:08 +0330

    Update ApplicationServices.cs

--- Identity/Identity.Service/Services/ApplicationServices.cs
+++ Identity/Identity.Service/Services/ApplicationServices.cs
@@ -1,6 +1,7 @@
 ﻿using Identity.Entity.User;
 using Identity.Service.Rules;
 using Identity.Services.Base;
+using Identity.Tools.File;
 
 namespace Identity.Service.Services;
 
@@ -16,11 +17,30 @@ public class ApplicationServices : IApplicationRules, IDisposable
         _account = account;
     }
 
+    public async Task<CUApplicationResponse> CreateApplicationAsync(CUApplicationViewModel application, HttpContext context)
+        => await Task.Run(async () =>
+        {
+            User user = await _account.GetUserAsync(context);
+            if (user != null)
+            {
+                IEnumerable<Application> userApplications = await _applicationCrud.GetAsync(app => app.OwnerId == user.Id);
+                if (!userApplications.Any(ua => ua.Name == application.Name))
+                {
+                    Application newApplication = await CreateApplicationAsync(application, user);
+                    return await _applicationCrud.InsertAsync(newApplication) ?
+                        new CUApplicationResponse(CUApplicationStatus.Success, await CreateApplicationViewModelAsync(newApplication))
+                            : new CUApplicationResponse(CUApplicationStatus.Exception, null);
+                }
+                return new CUApplicationResponse(CUApplicationStatus.ApplicationExist, null);
+            }
+            return new CUApplicationResponse(CUApplicationStatus.UserNotFound, null);
+        });
+
     public async Task<IEnumerable<ApplicationViewModel>> CreateApplicationViewModelAsync(IEnumerable<Application> applications)
      => await Task.FromResult(applications.Select(app => CreateApplicationViewModelAsync(app).Result));
 
     public async Task<ApplicationViewModel> CreateApplicationViewModelAsync(Application application)
-        => await Task.Run(() => new ApplicationViewModel(application.Id, application.Name, application.Image, application.ApiKey));
+        => await Task.Run(() => new ApplicationViewModel(application.Id, application.Name, application.Image.CreateFileAddress("application"), application.ApiKey));
 
     public void Dispose()
     {
@@ -40,6 +60,11 @@ public class ApplicationServices : IApplicationRules, IDisposable
          return new GetApplicationsResponse(GetApplicationsStatus.UserNotFound, null);
      });
 
+    public Task<GetApplicationsResponse> GetApplcationsAsync(HttpContext context)
+    {
+        throw new NotImplementedException();
+    }
+
     public Task GetApplicationAsync(Guid appId, HttpContext context)
     {
         throw new NotImplementedException();
@@ -55,6 +80,24 @@ public class ApplicationServices : IApplicationRules, IDisposable
         throw new NotImplementedException();
     }
 
-   
+    private static async Task<Application> CreateApplicationAsync(CUApplicationViewModel cuApp, User user)
+        => await Task.Run(async () =>
+                    {
+                        string img = "null.png";
+                        if (cuApp.File != null)
+                        {
+                            cuApp.File.Path = "application";
+                            img = await cuApp.File.SaveFileBase64Async();
+                        }
+
+                        return new Application()
+                        {
+                            ApiKey = Guid.NewGuid().ToString().CreateSHA256(),
+                            Password = cuApp.Password.CreateSHA256(),
+                            OwnerId = user.Id,
+                            Name = cuApp.Name,
+                            Image = img
+                        };
+                    });
 }
 
