a0c716e62aa86c594fd43d22f9b9cae42c781404
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-12-10 20:27:06 +0330

    Create File.Tool.cs

---
+++ Identity/Identity.Tools/File/File.Tool.cs
@@ -0,0 +1,38 @@
+﻿using Identity.ViewModels.File;
+
+namespace Identity.Tools.File;
+
+public static class FileExtensions
+{
+    public static async Task<string?> SaveFileBase64Async(this FileBase64ViewModel fileBase64)
+        => await Task.Run(async () =>
+        {
+            byte[] buffer = Convert.FromBase64String(fileBase64.Base64);
+            return await new FileBytesViewModel(buffer)
+            {
+                Extension = fileBase64.Extension,
+                Path = fileBase64.Path
+            }.SaveFileBytesAsync();
+        });
+
+    public static async Task<string?> SaveFileBytesAsync(this FileBytesViewModel fileBytes)
+        => await Task.Run(async () =>
+        {
+            try
+            {
+                string? path = Directory.GetCurrentDirectory() + $"/wwwroot/files/{fileBytes.Path}";
+                string fileName = Guid.NewGuid() + fileBytes.Extension;
+                if (!Directory.Exists(path))
+                    Directory.CreateDirectory(path);
+                await WriteAllBytesAsync(path + $"/{fileName}", fileBytes.Bytes);
+                return fileName;
+            }
+            catch
+            {
+                return "null.png";
+            }
+        });
+
+    public static string CreateFileAddress(this string fileName, string path)
+        => $"https://localhost:44343/files/{path}/{fileName}";
+}
