96c2cdf3230658625bf41741963334c8edeec48b
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-12-10 18:52:10 +0330

    Update Application.cs

--- Identity/Identity.ViewModels/Application/Application.cs
+++ Identity/Identity.ViewModels/Application/Application.cs
@@ -3,3 +3,5 @@
 public record ApplicationViewModel(Guid Id,string Name, string Image, string ApiKey);
 
 public record ApplicationRequest(string ApiKey,string Password);
+
+public record CUApplicationViewModel(Guid? Id,string Name,FileViewModel File);
