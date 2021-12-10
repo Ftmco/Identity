eb046526d9f954aac28be20bc675f98b81c76354
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-12-10 11:51:44 +0330

    Update IAccountRules.cs

--- Identity/Identity.Service/Rules/IAccountRules.cs
+++ Identity/Identity.Service/Rules/IAccountRules.cs
@@ -1,10 +1,20 @@
-﻿namespace Identity.Service.Rules;
+﻿using Identity.Entity.User;
+
+namespace Identity.Service.Rules;
 
 public interface IAccountRules
 {
     Task<SignUpResponse> SignUpAsync(SignUpViewModel signUp);
 
     Task<LoginResponse> LoginAsync(LoginViewModel login);
+
+    Task<User> GetUserAsync(HttpContext context);
+
+    Task<User> GetUserAsync(string userName);
+
+    Task<bool> CheckPasswordAsync(User user,string password);
+
+    Task<bool> CheckPasswordAsync(string userName,string password);
 
     Task<ChangePasswordStatus> ChangePasswordAsync(ChangePasswordViewModel changePassword);
 }
