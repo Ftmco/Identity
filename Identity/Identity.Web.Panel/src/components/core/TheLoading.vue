b9a9ba33d554d3be919221988fd53bf978f7fbbd
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-12-10 15:39:18 +0330

    create core components

---
+++ Identity/Identity.Web.Panel/src/components/core/TheLoading.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-dialog v-model="dialog" hide-overlay persistent width="300">
+    <v-card color="primary" dark>
+      <v-card-text>
+        لطفا منتظر بمانید
+        <v-progress-linear
+          indeterminate
+          color="white"
+          class="mb-0"
+        ></v-progress-linear>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    dialog: false,
+  }),
+  created() {
+    this.$root.$refs.loading = this;
+  },
+  methods: {
+    open() {
+      this.dialog = true;
+    },
+    close() {
+      this.dialog = false;
+    },
+  },
+};
+</script>
