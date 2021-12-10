6205774ca643d6b13e3b396320063596110bb982
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-12-10 15:36:36 +0330

    Update ApplicationController.cs

--- Identity/Identity.Web.Server/Controllers/Application/ApplicationController.cs
+++ Identity/Identity.Web.Server/Controllers/Application/ApplicationController.cs
@@ -1,4 +1,6 @@
-﻿namespace Identity.Web.Server.Controllers.Application;
+﻿using Identity.ViewModels.Application;
+
+namespace Identity.Web.Server.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
@@ -14,8 +16,14 @@ public class ApplicationController : ControllerBase
     [HttpGet("GetApplications")]
     public async Task<IActionResult> GetApplications(int page, int count)
     {
-        _application.GetApplcationsAsync(page, count, HttpContext);
-        return Ok();
+        GetApplicationsResponse? applications = await _application.GetApplcationsAsync(page, count, HttpContext);
+        return applications.Status switch
+        {
+            GetApplicationsStatus.Success => Ok(Success("Applications", "", new { applications.Applications })),
+            GetApplicationsStatus.UserNotFound => Ok(Notfound("User Notfound","Please Login to See your applications")),
+            GetApplicationsStatus.Exception => Ok(Excetpion("Exception","Please Try again to get applications")),
+            _ => Ok(Excetpion("Exception", "Please Try again to get applications")),
+        };
     }
 }
 
