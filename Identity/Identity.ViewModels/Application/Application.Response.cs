7cb4f0d15f9484a91e341188e7a2e11b2281348d
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-12-10 20:27:20 +0330

    Update Application.Response.cs

--- Identity/Identity.ViewModels/Application/Application.Response.cs
+++ Identity/Identity.ViewModels/Application/Application.Response.cs
@@ -1,6 +1,8 @@
 ﻿namespace Identity.ViewModels.Application;
 
-public record GetApplicationsResponse(GetApplicationsStatus Status,IEnumerable<ApplicationViewModel>? Applications);
+public record GetApplicationsResponse(GetApplicationsStatus Status, IEnumerable<ApplicationViewModel>? Applications);
+
+public record CUApplicationResponse(CUApplicationStatus Status, ApplicationViewModel Application);
 
 
 public enum GetApplicationsStatus
@@ -8,4 +10,13 @@ public enum GetApplicationsStatus
     Success = 0,
     UserNotFound = 1,
     Exception = 2
+}
+
+public enum CUApplicationStatus
+{
+    Success = 0,
+    UserNotFound = 1,
+    Exception = 2,
+    ApplicationNotFound = 3,
+    ApplicationExist = 4
 }
