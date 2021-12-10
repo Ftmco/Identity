fc7e8c709955716634c06c942ea9bba48a39b1bf
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-12-10 20:27:31 +0330

    Update ApplicationController.cs

--- Identity/Identity.Web.Server/Controllers/Application/ApplicationController.cs
+++ Identity/Identity.Web.Server/Controllers/Application/ApplicationController.cs
@@ -20,9 +20,25 @@ public class ApplicationController : ControllerBase
         return applications.Status switch
         {
             GetApplicationsStatus.Success => Ok(Success("Applications", "", new { applications.Applications })),
-            GetApplicationsStatus.UserNotFound => Ok(Notfound("User Notfound","Please Login to See your applications")),
-            GetApplicationsStatus.Exception => Ok(Excetpion("Exception","Please Try again to get applications")),
+            GetApplicationsStatus.UserNotFound => Ok(Notfound("User Notfound", "Please Login to See your applications")),
+            GetApplicationsStatus.Exception => Ok(Excetpion("Exception", "Please Try again to get applications")),
             _ => Ok(Excetpion("Exception", "Please Try again to get applications")),
+        };
+    }
+
+    [HttpPost("Create")]
+    public async Task<IActionResult> Create(CUApplicationViewModel create)
+    {
+        CUApplicationResponse? createApplication = await _application.CreateApplicationAsync(create, HttpContext);
+
+        return createApplication.Status switch
+        {
+            CUApplicationStatus.Success => Ok(Success($"Success To Create Application : {create.Name}", "", new { createApplication.Application })),
+            CUApplicationStatus.UserNotFound => Ok(Notfound("User Notfound", "Please Login to See your applications")),
+            CUApplicationStatus.Exception => Ok(Excetpion("Exception", "Please Try again to create applications")),
+            CUApplicationStatus.ApplicationNotFound => Ok(Notfound("Application Notfound", "Application is not exist")),
+            CUApplicationStatus.ApplicationExist => Ok(AccessDenied("Application Has Exist", $"An application has exist with this name : {create.Name}")),
+            _ => Ok(Excetpion("Exception", "Please Try again to create applications")),
         };
     }
 }
