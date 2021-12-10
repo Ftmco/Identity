1f116d32087f6159682ff69ab70cde1a3204e7fd
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-12-10 11:53:02 +0330

    Create ApplicationController.cs

---
+++ Identity/Identity.Web.Server/Controllers/Application/ApplicationController.cs
@@ -0,0 +1,21 @@
+﻿namespace Identity.Web.Server.Controllers.Application;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ApplicationController : ControllerBase
+{
+    private readonly IApplicationRules _application;
+
+    public ApplicationController(IApplicationRules application)
+    {
+        _application = application;
+    }
+
+    [HttpGet("GetApplications")]
+    public async Task<IActionResult> GetApplications(int page, int count)
+    {
+        _application.GetApplcationsAsync(page, count, HttpContext);
+        return Ok();
+    }
+}
+
