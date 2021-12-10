0a63a43e219ac2080947f56f272347924bd00a94
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-12-10 11:52:02 +0330

    Create application service

---
+++ Identity/Identity.Service/Rules/IApplicationRules.cs
@@ -0,0 +1,16 @@
+﻿namespace Identity.Service.Rules;
+
+public interface IApplicationRules
+{
+    Task<GetApplicationsResponse> GetApplcationsAsync(int page, int count, HttpContext context);
+
+    Task<IEnumerable<ApplicationViewModel>> CreateApplicationViewModelAsync(IEnumerable<Application> applications);
+
+    Task<ApplicationViewModel> CreateApplicationViewModelAsync(Application application);
+
+    Task GetApplicationAsync(Guid appId,HttpContext context);
+
+    Task GetApplicationAsync(string apiKey);
+
+    Task GetApplicationAsync(string apiKey,HttpContext context);
+}
---
+++ Identity/Identity.Service/Services/ApplicationServices.cs
@@ -0,0 +1,58 @@
+﻿using Identity.Entity.User;
+using Identity.Service.Rules;
+using Identity.Services.Base;
+
+namespace Identity.Service.Services;
+
+public class ApplicationServices : IApplicationRules, IDisposable
+{
+    private readonly IBaseRules<Application> _applicationCrud;
+
+    private readonly IAccountRules _account;
+
+    public ApplicationServices(IBaseRules<Application> applicationCrud, IAccountRules account)
+    {
+        _applicationCrud = applicationCrud;
+        _account = account;
+    }
+
+    public async Task<IEnumerable<ApplicationViewModel>> CreateApplicationViewModelAsync(IEnumerable<Application> applications)
+     => await Task.FromResult(applications.Select(app => CreateApplicationViewModelAsync(app).Result));
+
+    public async Task<ApplicationViewModel> CreateApplicationViewModelAsync(Application application)
+        => await Task.Run(() => new ApplicationViewModel(application.Name, application.Image, application.ApiKey));
+
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+    }
+
+    public async Task<GetApplicationsResponse> GetApplcationsAsync(int page, int count, HttpContext context)
+     => await Task.Run(async () =>
+     {
+         User? user = await _account.GetUserAsync(context);
+         if (user == null)
+         {
+             IEnumerable<Application>? userApplications = await _applicationCrud.GetAsync(app => app.OwnerId == user.Id);
+             IEnumerable<ApplicationViewModel>? applicationViewModel = await CreateApplicationViewModelAsync(userApplications);
+             return new GetApplicationsResponse(GetApplicationsStatus.Success, applicationViewModel);
+         }
+         return new GetApplicationsResponse(GetApplicationsStatus.UserNotFound, null);
+     });
+
+    public Task GetApplicationAsync(Guid appId, HttpContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task GetApplicationAsync(string apiKey)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task GetApplicationAsync(string apiKey, HttpContext context)
+    {
+        throw new NotImplementedException();
+    }
+}
+
