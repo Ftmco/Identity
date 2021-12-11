da4d234b189079bff8cbeb680ee85e92f9905b6a
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-12-11 16:32:14 +0330

    Create Code.Tools.cs

---
+++ Identity/Identity.Tools/Code/Code.Tools.cs
@@ -0,0 +1,7 @@
+﻿namespace Identity.Tools.Code;
+
+public static class CodeExtensions
+{
+    public static string CreateCode(this int length)
+           => Guid.NewGuid().GetHashCode().ToString()[..length];
+}
