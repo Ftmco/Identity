2172ca9e60c20b759fd06abfc82e3b6f819033a8
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-12-11 16:32:49 +0330

    Create ProfileController.cs

---
+++ Identity/Identity.Web.Server/Controllers/Account/ProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Web.Server.Controllers.Account;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProfileController : ControllerBase
+{
+
+}
+
