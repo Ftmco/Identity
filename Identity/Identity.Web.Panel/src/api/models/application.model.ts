65b8011f9f5736a3ca8bf9a2fda405944650bf5f
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-12-11 13:06:49 +0330

    update application model

--- Identity/Identity.Web.Panel/src/api/models/application.model.ts
+++ Identity/Identity.Web.Panel/src/api/models/application.model.ts
@@ -4,6 +4,14 @@ export type CreateApp = {
     file: File
 }
 
+export type UpdateApp = {
+    id: string;
+    name: string;
+    password: string;
+    file: File
+}
+
+
 export type File = {
     base64: string;
     extension: string;
