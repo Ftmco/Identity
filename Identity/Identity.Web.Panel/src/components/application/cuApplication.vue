565ba20f9693c7efd22e497b65899f5de148cd79
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-12-11 13:07:25 +0330

    check edit or update application for api call

--- Identity/Identity.Web.Panel/src/components/application/cuApplication.vue
+++ Identity/Identity.Web.Panel/src/components/application/cuApplication.vue
@@ -64,6 +64,7 @@
         data: () => ({
             applicationService: new ApplicationService(apiCall),
             application: {
+                id: "",
                 name: "",
                 password: "",
                 file: {
@@ -75,7 +76,8 @@
             rules: rules,
             show: false,
             btnText: "Create",
-            btnColor: "primary"
+            btnColor: "primary",
+            isEdit: false
         }),
         props: {
             editApp: Object
@@ -88,6 +90,23 @@
                 if (this.editApp) {
                     this.application.name = this.editApp.name;
                     this.img = this.editApp.image
+                    this.btnText = "Edit"
+                    this.btnColor = "warning"
+                    this.isEdit = true
+                } else {
+                    this.application = {
+                        id: "",
+                        file: {
+                            base64: "",
+                            extension: ""
+                        },
+                        name: "",
+                        password: ""
+                    }
+                    this.img = ""
+                    this.btnText = "Create"
+                    this.btnColor = "primary"
+                    this.isEdit = false
                 }
             },
             profileSelect(file: any) {
@@ -105,28 +124,52 @@
                 if (isValid) {
                     this.$root.$refs.loading.open()
                     this.application.file = this.createFile()
-                    this.applicationService.Create(this.application)
-                        .then((res) => {
-                            if (res.status)
-                                this.$emit("submit", {
-                                    app: res.result.application
-                                })
-                            this.showMessage(res.title)
-                        }).catch((e) => {
-                            this.showMessage(messages.netWorkError(e.message).title)
-                        })
+                    if (this.isEdit) {
+                        this.application.id = this.editApp.id
+                        this.update(this.application)
+                    } else
+                        this.create(this.application)
                 }
                 else
                     this.showMessage(messages.invalidForm)
             },
-            createFile() {
-                let base64items = this.img.split(";")
-                let extension = "." + base64items[0].split("/").pop();
-                let base64 = base64items[1].split(",").pop();
-                return {
-                    base64: base64,
-                    extension: extension
+            update(app: any) {
+                this.applicationService.Update(app).
+                    then((res) => {
+                        if (res.status)
+                            this.$emit("submit", {
+                                update: true,
+                                app: res.result.application
+                            })
+                        this.showMessage(res.title)
+                    }).catch((e) => {
+                        this.showMessage(messages.netWorkError(e.message).title)
+                    })
+            },
+            create(app: any) {
+                app.id = null;
+                this.applicationService.Create(app)
+                    .then((res) => {
+                        if (res.status)
+                            this.$emit("submit", {
+                                app: res.result.application
+                            })
+                        this.showMessage(res.title)
+                    }).catch((e) => {
+                        this.showMessage(messages.netWorkError(e.message).title)
+                    })
+            },
+            createFile(): any {
+                if (this.img.indexOf("http") == -1) {
+                    let base64items = this.img.split(";")
+                    let extension = "." + base64items[0].split("/").pop();
+                    let base64 = base64items[1].split(",").pop();
+                    return {
+                        base64: base64?.toString(),
+                        extension: extension
+                    }
                 }
+                return null
             },
             showMessage(text: string) {
                 this.$root.$refs.snackbar.open(text)
