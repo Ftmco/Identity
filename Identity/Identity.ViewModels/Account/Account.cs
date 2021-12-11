cc5c605b0141e5f78d434636bce02e7647b8776f
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2021-12-11 16:32:23 +0330

    Update Account.cs

--- Identity/Identity.ViewModels/Account/Account.cs
+++ Identity/Identity.ViewModels/Account/Account.cs
@@ -2,8 +2,8 @@
 
 namespace Identity.ViewModels.Account;
 
-public record SignUpViewModel(string UserName, string FullName, string Password, string? Email, string? MobileNo, ApplicationRequest? Application);
+public record SignUpViewModel(string UserName, string FullName, string Password, string? Email, string? MobileNo,ApplicationRequest? Application);
 
 public record LoginViewModel(string UserName, string Password, ApplicationRequest? Application);
 
-public record ChangePasswordViewModel(string CurrentPassword, string NewPassword, ApplicationRequest? Application);
+public record ChangePasswordViewModel(string CurrentPassword, string NewPassword);
