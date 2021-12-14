b68b0bde4b632ed725d2d9fbf4a68d7c5f9b06f4
Amir Hossein <amir0hossin0pr@gmail.com>
2021-12-14 03:33:26 -0800

    Update Account.cs

--- Identity/Identity.ViewModels/Account/Account.cs
+++ Identity/Identity.ViewModels/Account/Account.cs
@@ -9,3 +9,7 @@ public record LoginViewModel(string UserName, string Password, ApplicationReques
 public record ChangePasswordViewModel(string CurrentPassword, string NewPassword);
 
 public record UserViewModel(Guid Id,string UserName,string FullName,string Email,string MobileNo,DateTime RegisterDate);
+
+public record ForgotPasswordViewModel(string UserName);
+
+public record ResetPasswordViewModel(string UserName,string Code,string Password);
