10384394d5df62e45b87ed8ed28c638ca962eef1
Amir Hossein <amir0hossin0pr@gmail.com>
2021-12-14 03:33:50 -0800

    Update Account.Response.cs

--- Identity/Identity.ViewModels/Account/Account.Response.cs
+++ Identity/Identity.ViewModels/Account/Account.Response.cs
@@ -1,11 +1,10 @@
-﻿using Identity.Entity.User;
-namespace Identity.ViewModels.Account.Response;
+﻿namespace Identity.ViewModels.Account.Response;
 
-public record SignUpResponse(SignUpStatus Status, User User);
+public record SignUpResponse(SignUpStatus Status, UserViewModel User);
 
 public record LoginResponse(LoginStatus Status, SessionViewModel? Session);
 
-public record SessionViewModel(string Key, string Value,DateTime ExpireDate);
+public record SessionViewModel(string Key, string Value, DateTime ExpireDate);
 
 public enum SignUpStatus
 {
@@ -29,4 +28,12 @@ public enum ChangePasswordStatus
     WrongPassword = 1,
     Exception = 2,
     UserNotFound = 3
+}
+
+public enum ForgotPasswordStatus
+{
+    Success = 0,
+    UserNotFound = 1,
+    Exception = 2,
+    WrongCode = 3
 }
