6e408b3944264ef821d5e852e8d1edca96607466
Amir Hossein <amir0hossin0pr@gmail.com>
2021-12-14 03:29:14 -0800

    update middleware

--- Identity/Identity.Web.Panel/src/router/middleware/guest.ts
+++ Identity/Identity.Web.Panel/src/router/middleware/guest.ts
@@ -1,4 +1,4 @@
-export default function ({ store, next }) {
+export default function ({ store, next }: any) {
     if (store.getters.auth.isLogin) {
         return next({
             name: "Profile"
