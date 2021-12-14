b5157340ac80fb58ef342386c48ce11fb9a48caa
Amir Hossein <amir0hossin0pr@gmail.com>
2021-12-14 03:32:05 -0800

    Update GlobalUsings.cs

--- Identity/Identity.Web.Server/GlobalUsings.cs
+++ Identity/Identity.Web.Server/GlobalUsings.cs
@@ -1,9 +1,13 @@
 ﻿global using Identity.Context;
-global using Microsoft.EntityFrameworkCore;
-global using Microsoft.AspNetCore.Mvc;
 global using Identity.Service.Rules;
 global using Identity.Service.Services;
 global using Identity.ViewModels.Account;
 global using Identity.ViewModels.Account.Response;
+global using Identity.Entity.Application;
+global using Identity.Entity.Page;
+global using Identity.Entity.User;
+global using Identity.Services.Base;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.AspNetCore.Mvc;
 
 global using static Identity.ViewModels.Api.ApiResponse;
