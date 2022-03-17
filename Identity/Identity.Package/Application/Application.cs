1ff3aef4505acfef83b6939ceb5823f83ef45109
Amir Hossein <amir0hossin0pr@gmail.com>
2022-03-17 13:15:42 -0700

    delete lasted service of package server

--- Identity/Identity.Package/Application/Application.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Identity.Package.Application;
-
-public record Application(string ApiKey, string Password);
-
-public record User(Guid Id, string UserName, string FullName, string Email, string MobileNo, DateTime RegisterDate);
-
-public record UserResponse(IEnumerable<User> Users);
