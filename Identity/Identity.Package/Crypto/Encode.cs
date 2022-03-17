d12314b07307eb2916827151fcefd4bab499961f
Amir Hossein <amir0hossin0pr@gmail.com>
2022-03-17 13:16:34 -0700

    Create Encode.cs

---
+++ Identity/Identity.Package/Crypto/Encode.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Text;
+
+namespace Tools.Crypto;
+
+public static class Encode
+{
+    public static string Encrypt(this string text, string key, bool revert = false)
+    {
+        string newString = "";
+        for (int i = 0; i < text.Length; i++)
+            newString += (char)(text[i] + (revert ? key[(Math.Abs(key.Length - i) % key.Length)] : key[(i % key.Length)]));
+
+        return newString;
+    }
+
+    public static string Decrypt(this string text, string key, bool revert = false)
+    {
+        string newString = "";
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            int code = (text[i] - (revert ? key[(Math.Abs(key.Length - i) % key.Length)] : key[(i % key.Length)]));
+            newString += (char)code;
+        }
+
+        return newString;
+    }
+
+    public static string KeyMaker(this string path)
+    {       
+        byte[]? bytes = Encoding.UTF8.GetBytes(path);
+        string? base64 = Convert.ToBase64String(bytes);
+        return base64.ToString();
+    }
+}
