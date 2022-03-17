0ccceb089239bf04bbede727ee840496c95d73b2
Amir Hossein <amir0hossin0pr@gmail.com>
2022-03-17 13:17:00 -0700

    create user model for package

---
+++ Identity/Identity.Package/Models/User.cs
@@ -0,0 +1,28 @@
+﻿namespace Identity.Entity.User;
+
+public record User
+{
+    public Guid Id { get; set; }
+
+    public string FullName { get; set; }
+
+    public string UserName { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public string Email { get; set; }
+
+    public string MobileNo { get; set; }
+
+    public DateTime RegisterDate { get; set; }
+}
+
+public record GetAllUsers(ActionStatus Status,IEnumerable<User> Users);
+
+public record GetUser(ActionStatus Status,User User);
+
+public enum ActionStatus
+{
+    Success = 0,
+    Exception = 1
+}
