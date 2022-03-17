bf94ee278dd72fa85027cdc1e20d59240f04aa2c
Amir Hossein <amir0hossin0pr@gmail.com>
2022-03-17 13:16:50 -0700

    Create UserController.cs

---
+++ Identity/Identity.Web.Server/Controllers/Application/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Identity.ViewModels.Application;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Web.Server.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    [HttpPost("GetUserByToken")]
+    public async Task<IActionResult> GetUserByToken(ApiRequest token)
+    {
+        var model = token.ReadRequestDataAsync<GetUserByToken>(HttpContext);
+        return Ok();
+    }
+
+    [HttpPost("GetUserByUserName")]
+    public async Task<IActionResult> GetUserByUserName(ApiRequest userName)
+    {
+        var model = userName.ReadRequestDataAsync<GetUserByUserName>(HttpContext);
+        return Ok();
+    }
+}
