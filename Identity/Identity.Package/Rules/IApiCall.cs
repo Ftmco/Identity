66c885ff939033086ea9033769b6145015a502dc
Amir Hossein <amir0hossin0pr@gmail.com>
2022-03-17 13:16:27 -0700

    create api call service for package

---
+++ Identity/Identity.Package/Rules/IApiCall.cs
@@ -0,0 +1,15 @@
+﻿using Identity.Package.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Package.Rules;
+
+public interface IApiCall : IAsyncDisposable
+{
+    Task<string> PostAsync<TValue>(string url,TValue value);
+
+    Task<string> GetAsync(string url);
+}
