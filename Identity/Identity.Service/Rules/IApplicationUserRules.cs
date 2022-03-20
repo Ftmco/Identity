8258276d71ab9c44c68d081a302da93cf7574906
Amir Hossein <amir0hossin0pr@gmail.com>
2022-03-20 03:33:48 -0700

    create application user services

---
+++ Identity/Identity.Service/Rules/IApplicationUserRules.cs
@@ -0,0 +1,8 @@
+﻿namespace Identity.Service.Rules;
+
+public interface IApplicationUserRules : IAsyncDisposable
+{
+    Task<GetUserResponse> GetUserByTokenAsync(GetUserByToken getUserByToken);
+
+    Task<GetUserResponse> GetUserByUserNameAsync(GetUserByUserName getUserByUserName);
+}
