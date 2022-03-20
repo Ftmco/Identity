1c604116f27cef50e14825741477ecdb98350bd6
Amir Hossein <amir0hossin0pr@gmail.com>
2022-03-20 03:34:42 -0700

    Update User.cs

--- Identity/Identity.ViewModels/Application/User.cs
+++ Identity/Identity.ViewModels/Application/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Identity.ViewModels.Account;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,19 @@ namespace Identity.ViewModels.Application;
 
 public record GetUserViewModel
 {
-    public ApplicationRequest Application { get; set; }
+    public ApplicationRequest? Application { get; set; }
 }
 
 public record GetUserByToken(string Token) : GetUserViewModel;
 
 public record GetUserByUserName(string UserName) : GetUserViewModel;
+
+public record GetUserResponse(GetUserStatus Status,UserViewModel User);
+
+public enum GetUserStatus
+{
+    Success = 0,
+    ApplicationNotFound = 1,
+    Exception = 2,
+    UserNotFound = 3,
+}
