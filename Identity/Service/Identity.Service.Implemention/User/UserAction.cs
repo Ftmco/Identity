dfe3a1d5ae792fefdbfd35a86f695c7b4937cda7
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-04-06 11:40:50 -0700

    user service

--- Identity/Service/Identity.Service.Implemention/User/UserAction.cs
+++ Identity/Service/Identity.Service.Implemention/User/UserAction.cs
@@ -1,20 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Identity.DataBase.Entity;
+using Identity.DataBase.ViewModel.Account;
+using Identity.Service.Tools.Code;
+using Identity.Service.Tools.Crypto;
 
 namespace Identity.Service.Implemention;
 
 public class UserAction : IUserAction
 {
-    public Task CreateUserAsync()
+    readonly IBaseCud<User, IdentityContext> _userCud;
+
+    readonly IBaseQuery<User, IdentityContext> _userQuery;
+
+    readonly IUserGet _userGet;
+
+    public UserAction(IBaseCud<User, IdentityContext> userCud, IBaseQuery<User, IdentityContext> userQuery, IUserGet userGet)
     {
-        throw new NotImplementedException();
+        _userCud = userCud;
+        _userQuery = userQuery;
+        _userGet = userGet;
     }
 
-    public ValueTask DisposeAsync()
+    public async Task<User?> CreateUserAsync(SignUp signUp)
     {
-        throw new NotImplementedException();
+        User newUser = new()
+        {
+            Email = " ",
+            FullName = signUp.FullName,
+            MobileNo = signUp.MobileNo,
+            IsActvie = false,
+            Password = signUp.Password.CreateSHA256(),
+            RegisterDate = DateTime.UtcNow,
+            UserName = " ",
+            ActiveCode = 5.CreateCode()
+        };
+        return await _userCud.InsertAsync(newUser) ? newUser : null;
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        GC.SuppressFinalize(this);
+        await _userGet.DisposeAsync();
     }
 }
--- Identity/Service/Identity.Service.Implemention/User/UserGet.cs
+++ Identity/Service/Identity.Service.Implemention/User/UserGet.cs
@@ -1,15 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Identity.DataBase.Entity;
 
 namespace Identity.Service.Implemention;
 
 public class UserGet : IUserGet
 {
+    readonly IBaseQuery<User, IdentityContext> _userQuery;
+
+    public UserGet(IBaseQuery<User, IdentityContext> userQuery)
+    {
+        _userQuery = userQuery;
+    }
+
     public ValueTask DisposeAsync()
     {
         throw new NotImplementedException();
+    }
+
+    public async Task<User?> GetUserByUserNameAsync(string userName)
+    {
+        return await _userQuery.GetAsync(u => u.MobileNo == userName || u.Email == userName || u.UserName == userName);
     }
 }
