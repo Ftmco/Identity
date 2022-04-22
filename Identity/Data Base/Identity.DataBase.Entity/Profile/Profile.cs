d6ce42f0b58311fa40792c4e6e9fc74f6ec3919f
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-04-22 05:33:56 -0700

    create profile entities

---
+++ Identity/Data Base/Identity.DataBase.Entity/Profile/Profile.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.DataBase.Entity;
+
+public record Profile
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [Required]
+    public Guid UserId { get; set; }
+
+    [Required]
+    public string FistName { get; set; }
+
+    [Required]
+    public string LastName { get; set; }
+
+    //Navigation Property
+    //Relationships
+
+    public virtual ICollection<ProfileImage> ProfileImages { get; set; }
+}
