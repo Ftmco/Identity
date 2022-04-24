acf73993af362210398617d1a16363eacdb65151
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-04-24 06:20:11 -0700

    Update AccountService.cs

--- Identity/Server/Identity.Server.Grpc/Services/AccountService.cs
+++ Identity/Server/Identity.Server.Grpc/Services/AccountService.cs
@@ -9,12 +9,15 @@ public class AccountService : Account.AccountBase
 {
     readonly IAccountAction _accountAction;
 
+    readonly IProfileGet _profileGet;
+
     readonly IUserGet _userGet;
 
-    public AccountService(IAccountAction accountAction, IUserGet userGet)
+    public AccountService(IAccountAction accountAction, IUserGet userGet, IProfileGet profileGet)
     {
         _accountAction = accountAction;
         _userGet = userGet;
+        _profileGet = profileGet;
     }
 
     public override async Task<GetUserReply> GetUser(GetUserRequest request, ServerCallContext context)
@@ -43,5 +46,26 @@ public class AccountService : Account.AccountBase
             GetUserStatus.Exception => new GetUserReply { Code = 500, Status = false, Title = "خطایی رخ داد مجددا تلاش کنید", Message = "", Result = null },
             _ => new GetUserReply { Code = 500, Status = false, Title = "خطایی رخ داد مجددا تلاش کنید", Message = "", Result = null },
         };
+    }
+
+    public override async Task GetUsers(IAsyncStreamReader<GetUserById> requestStream, IServerStreamWriter<UserModel> responseStream, ServerCallContext context)
+    {
+        await foreach (GetUserById? rs in requestStream.ReadAllAsync())
+        {
+            User? user = await _userGet.GetUserAsync(rs.Id);
+            if (user != null)
+            {
+                ProfileResponse? profile = await _profileGet.GetProfileAsync(user.Id);
+                await responseStream.WriteAsync(new()
+                {
+                    Email = user.Email,
+                    Id = user.Id.ToString(),
+                    FullName = profile.Status == ProfileStatus.Success ? $"{profile.Profile.FirstName} {profile.Profile.LastName}" : "",
+                    IsActive = user.IsActvie,
+                    MobileNo = user.MobileNo,
+                    RegisterDate = user.RegisterDate.ToString()
+                });
+            }
+        }
     }
 }
