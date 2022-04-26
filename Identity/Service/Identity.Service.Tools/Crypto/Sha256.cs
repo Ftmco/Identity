7d96961217ce869c87b82e097cf798e7f14670d2
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-04-26 11:02:20 -0700

    remove nullable array

--- Identity/Service/Identity.Service.Tools/Crypto/Sha256.cs
+++ Identity/Service/Identity.Service.Tools/Crypto/Sha256.cs
@@ -8,7 +8,7 @@ public static class CryptoUtils
     public static string CreateSHA256(this string str)
     {
         using SHA256 sha256 = SHA256.Create();
-        byte[]? bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(str));
+        byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(str));
         StringBuilder stringBuilder = new();
         bytes.ToList().ForEach((b) => stringBuilder.Append(b.ToString("x2")));
         return stringBuilder.ToString();
