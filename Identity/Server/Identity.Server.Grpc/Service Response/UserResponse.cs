58c420680707b7f62bbd99f239764ddc397bf226
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-04-26 11:04:52 -0700

    response generator for user service

---
+++ Identity/Server/Identity.Server.Grpc/Service Response/UserResponse.cs
@@ -0,0 +1,19 @@
+﻿namespace Identity.Server.Grpc.ServiceResponse;
+
+public static class UserResponse
+{
+    public static GetUserReply Success()
+    {
+        return new();
+    }
+
+    public static GetUserReply NotFound()
+    {
+        return new();
+    }
+
+    public static GetUserReply Exception()
+    {
+        return new();
+    }
+}
