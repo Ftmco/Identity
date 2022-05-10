f66ce4787281f448f24678822afa0818ba167ca0
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-05-10 06:05:17 -0700

    change account service name

---
+++ Identity/Package/Identity.Client/Rules/Account/IAccountAction.cs
@@ -0,0 +1,9 @@
+﻿using Identity.Client.Models;
+using Identity.Server.Grpc.Protos;
+
+namespace Identity.Client.Rules;
+
+public interface IAccountAction : IAsyncDisposable
+{
+    Task<LoginReply> LoginAsync(LoginRequest login);
+}
---
+++ Identity/Package/Identity.Client/Services/Account/AccountAction.cs
@@ -0,0 +1,27 @@
+﻿using Identity.Server.Grpc.Protos;
+
+namespace Identity.Client.Services;
+
+public class AccountAction : IAccountAction
+{
+    readonly IGrpcRule _gRPC;
+
+    public AccountAction(IGrpcRule gRPC)
+    {
+        _gRPC = gRPC;
+    }
+
+    public ValueTask DisposeAsync()
+    {
+        GC.SuppressFinalize(this);
+        return ValueTask.CompletedTask;
+    }
+
+    public async Task<LoginReply> LoginAsync(LoginRequest login)
+    {
+        GrpcChannel? grpc = await _gRPC.OpenChannelAsync();
+        Account.AccountClient? client = new(grpc);
+        LoginReply? request = await client.LoginAsync(login);
+        return request;
+    }
+}
