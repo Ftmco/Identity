845bde196f7aa9fd074c5cc8f7bd6ab295b700ee
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-05-10 06:05:43 -0700

    Create AccountService.cs

---
+++ Identity/Server/Identity.Server.Grpc/Services/AccountService.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Core;
+using Identity.DataBase.ViewModel.Account;
+
+namespace Identity.Server.Grpc.Services;
+
+public class AccountService : Account.AccountBase
+{
+    readonly IAccountAction _account;
+
+    public AccountService(IAccountAction account)
+    {
+        _account = account;
+    }
+
+    public override async Task<LoginReply> Login(LoginRequest request, ServerCallContext context)
+    {
+        LoginResponse? loginUser = await _account.LoginAsync(new(UserName: request.UserName, Password: request.Password));
+        return new LoginReply
+        {
+            Status = (int)loginUser.Status,
+            Session = new() { Key = loginUser.Session?.Key ?? "", Value = loginUser.Session?.Value ?? "" }
+        };
+    }
+}
