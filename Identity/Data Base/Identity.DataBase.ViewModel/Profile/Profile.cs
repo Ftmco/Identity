ae7fdf69c80bce2063cadf5c499ba2e7e0a547fd
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-05-12 08:09:47 -0700

    Update Profile.cs

--- Identity/Data Base/Identity.DataBase.ViewModel/Profile/Profile.cs
+++ Identity/Data Base/Identity.DataBase.ViewModel/Profile/Profile.cs
@@ -6,13 +6,17 @@ using System.Threading.Tasks;
 
 namespace Identity.DataBase.ViewModel;
 
-public record ProfileViewModel(Guid ProfileId, Guid UserId, string FirstName, string LastName,UserViewModel User, IEnumerable<FileViewModel> Images);
+public record ProfileViewModel(Guid ProfileId, Guid UserId, string FirstName, string LastName, UserViewModel User, IEnumerable<FileViewModel> Images);
 
-public record UpdateProfile(string FirstName,string LastName,string Email,Guid? FileId,string? FileToken);
+public record UpdateProfile(string FirstName, string LastName, string Email, Guid? FileId, string? FileToken);
 
-public record FileViewModel(Guid FileId, string FileToken);
+public record FileViewModel(Guid FileId, string FileToken)
+{
+    public string Base64 { get; set; } = " ";
+}
 
-public record ProfileResponse(ProfileStatus Status,ProfileViewModel? Profile);
+
+public record ProfileResponse(ProfileStatus Status, ProfileViewModel? Profile);
 
 public enum ProfileStatus
 {
