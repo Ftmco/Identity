18c3792a4c892ed4eecf3f5564c13d426946e0e9
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-05-12 08:10:02 -0700

    Create Avatar.cs

---
+++ Identity/Package/Identity.Client/Models/Avatar.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Client.Models;
+
+public record Avatar(Guid FileId,string FileToken,string? Base64);
