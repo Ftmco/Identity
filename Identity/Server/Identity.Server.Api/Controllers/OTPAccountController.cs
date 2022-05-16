c704212d1efc5be92019eef2c5c1dea8a4b9fefe
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-05-16 02:33:50 -0700

    Create OTPAccountController.cs

---
+++ Identity/Server/Identity.Server.Api/Controllers/OTPAccountController.cs
@@ -0,0 +1,41 @@
+﻿using Identity.DataBase.ViewModel.Account;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Server.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class OTPAccountController : ControllerBase
+{
+    readonly IOtpAccountAction _otpAccount;
+
+    public OTPAccountController(IOtpAccountAction otpAccount)
+    {
+        _otpAccount = otpAccount;
+    }
+
+    [HttpPost("Login")]
+    public async Task<IActionResult> LoginAsync(FastLogin login)
+    {
+        return await _otpAccount.OtpLoginAsync(login) switch
+        {
+            LoginStatus.Success => Ok(Success("رمز یک بار مصرف برای شما ارسال شد", "", new { })),
+            LoginStatus.UserNotFound => throw new NotImplementedException(),
+            LoginStatus.Exception => Ok(ApiException("خطایی رخ داد مجددا تلاش کنید", "")),
+            _ => Ok(ApiException("خطایی رخ داد مجددا تلاش کنید", "")),
+        };
+    }
+
+    [HttpPost("Activation")]
+    public async Task<IActionResult> ActivationAsync(Activation activation)
+    {
+        var login = await _otpAccount.ActivationAsync(activation);
+        return login.Status switch
+        {
+            LoginStatus.Success => Ok(Success("ورود موفق بود", "", new { login.Session })),
+            LoginStatus.UserNotFound => Ok(Faild(404,"کاربری با این مشخصات یافت نشد","")),
+            LoginStatus.Exception => Ok(ApiException("خطایی رخ داد مجددا تلاش کنید", "")),
+            _ => Ok(ApiException("خطایی رخ داد مجددا تلاش کنید", "")),
+        };
+    }
+}
