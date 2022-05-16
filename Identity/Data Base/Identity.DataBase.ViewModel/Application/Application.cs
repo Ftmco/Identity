a0c93e6b1397d45db91ec609d1dbf7fab2a80f3b
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-05-16 05:35:24 -0700

    Create Application.cs

---
+++ Identity/Data Base/Identity.DataBase.ViewModel/Application/Application.cs
@@ -0,0 +1,9 @@
+﻿using Newtonsoft.Json;
+
+namespace Identity.DataBase.ViewModel;
+
+public record ApplicationViewModel(string Key, string ApiKey)
+{
+    public static ApplicationViewModel? CreateApplication(string json) =>
+    JsonConvert.DeserializeObject<ApplicationViewModel>(json);
+}
