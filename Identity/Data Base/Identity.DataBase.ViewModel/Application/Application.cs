3606ef753ce258e57c84315c69e8cca8256e42ce
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-05-17 00:44:33 -0700

    Update Application.cs

--- Identity/Data Base/Identity.DataBase.ViewModel/Application/Application.cs
+++ Identity/Data Base/Identity.DataBase.ViewModel/Application/Application.cs
@@ -7,3 +7,17 @@ public record ApplicationViewModel(string Key, string ApiKey)
     public static ApplicationViewModel? CreateApplication(string json) =>
     JsonConvert.DeserializeObject<ApplicationViewModel>(json);
 }
+
+public record UpsertApplication(Guid? Id, string Name);
+
+public record ApplicationInfo(Guid Id, string Name, string ApiKey, string Key, DateTime CreateDate, bool IsActive);
+
+public record UpsertApplicationResponse(ApplicationActionStatus Status, ApplicationInfo? Application);
+
+public enum ApplicationActionStatus
+{
+    Success = 0,
+    UserNotFound = 1,
+    Exception = 2,
+    ApplicationNotFound = 3
+}
