b432cfa167b8a52acb1634f5d4f9f8006d4922e5
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-05-17 00:45:03 -0700

    add application id for sessions

--- Identity/Service/Identity.Service.Abstraction/Session/ISessionAction.cs
+++ Identity/Service/Identity.Service.Abstraction/Session/ISessionAction.cs
@@ -1,8 +1,11 @@
-﻿namespace Identity.Service.Abstraction;
+﻿using System.Linq.Expressions;
+
+namespace Identity.Service.Abstraction;
 
 public interface ISessionAction : IAsyncDisposable
 {
-    Task<DataBase.Entity.Session?> CreateSessionAsync(User user);
+    Task<DataBase.Entity.Session?> CreateSessionAsync(User user, Guid appId);
 
     Task DeleteSessionAsync(string session);
+
 }
