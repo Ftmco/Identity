0b0da71491704fd65cb47da8b4b26a9ea3ba0cc0
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-05-17 00:44:39 -0700

    Create Convertor.cs

---
+++ Identity/Service/Identity.Service.Tools/Metadata/Convertor.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Core;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Service.Tools;
+
+public static class MetadataConvertor
+{
+    public static IHeaderDictionary ConvertToHeaderDictonary(this Metadata metadata)
+    {
+        IDictionary<string, StringValues> headers = new Dictionary<string, StringValues>();
+        foreach (var rHeader in metadata)
+            headers[rHeader.Key] = rHeader.Value;
+        return (IHeaderDictionary)headers;
+    }
+}
