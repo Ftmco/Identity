452011e3e05f4796fd5c01f4cbe91f898d473879
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-05-18 23:37:13 -0700

    page get services

---
+++ Identity/Service/Identity.Service.Abstraction/Page/IPageGet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Service.Abstraction;
+
+public interface IPageGet : IAsyncDisposable
+{
+    Task<Page?> GetPageByNameAsync(string name, Guid appId, bool actvie);
+
+    Task<IEnumerable<PagesRoles>> GetPageRolesAsync(Guid pageId);
+}
---
+++ Identity/Service/Identity.Service.Implemention/Page/PageGet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Service.Implemention;
+
+public class PageGet : IPageGet
+{
+    readonly IBaseQuery<Page, IdentityContext> _pageQuery;
+
+    readonly IBaseQuery<PagesRoles, IdentityContext> _pagesRolesQuery;
+
+    public PageGet(IBaseQuery<Page, IdentityContext> pageQuery, IBaseQuery<PagesRoles, IdentityContext> pagesRolesQuery)
+    {
+        _pageQuery = pageQuery;
+        _pagesRolesQuery = pagesRolesQuery;
+    }
+
+    public ValueTask DisposeAsync()
+    {
+        GC.SuppressFinalize(this);
+        return ValueTask.CompletedTask;
+    }
+
+    public async Task<Page?> GetPageByNameAsync(string name, Guid appId, bool active)
+        => await _pageQuery.GetAsync(p => p.Name == name && p.ApplicationId == appId && p.IsActive == active);
+
+    public async Task<IEnumerable<PagesRoles>> GetPageRolesAsync(Guid pageId)
+        => await _pagesRolesQuery.GetAllAsync(pr => pr.PageId == pageId);
+}
