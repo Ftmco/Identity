fbf2adc9df746ed4890d83860857f8cc38ba6c35
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-05-18 23:35:45 -0700

    Create AuthService.cs

---
+++ Identity/Server/Identity.Server.Grpc/Services/AuthService.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Core;
+
+namespace Identity.Server.Grpc.Services;
+
+public class AuthService : Auth.AuthBase
+{
+    readonly IApplicationAction _applicationAction;
+
+    public AuthService(IApplicationAction applicationAction)
+    {
+        _applicationAction = applicationAction;
+    }
+
+    public override async Task<CheckAccessReply> CheckAccess(CheckAccessRequest request, ServerCallContext context)
+    {
+        bool hasAccess = await _applicationAction.CheckUserAccessAsync(context.RequestHeaders,
+            new(UserSession: request.Session, Address: request.Address, PageName: request.PageName));
+
+        return new CheckAccessReply { HasAccess = hasAccess };
+    }
+}
