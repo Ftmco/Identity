9c53db5199c78b3115a957066e2ea0e2a8ec20bd
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-05-18 23:35:24 -0700

    Create Auth.cs

---
+++ Identity/Package/Identity.Authentication/Extension/Auth.cs
@@ -0,0 +1,33 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using Identity.Client.Models;
+using Identity.Server.Grpc.Protos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace Identity.Authentication.Extension;
+
+public static class AuthExtensions
+{
+    public static async Task<bool> CheckAccessAsync(this HttpContext context, string pageName, IConfiguration configuration)
+    {
+        var session = context.Request.Headers["Auth-Token"].ToString() ?? " ";
+        GrpcChannel channel = GrpcChannel.ForAddress(configuration["Identity:Address:gRPC"]);
+        Auth.AuthClient client = new(channel);
+
+        Metadata headers = new();
+        Application app = new(
+            ApiKey: configuration["Identity:Application:ApiKey"],
+            Key: configuration["Identity:Application:Key"]);
+        headers.Add("application", JsonConvert.SerializeObject(app));
+
+        CheckAccessReply checkAccess = await client.CheckAccessAsync(new()
+        {
+            Session = session,
+            Address = context.Request.Path,
+            PageName = pageName
+        }, headers);
+        return checkAccess.HasAccess;
+    }
+}
