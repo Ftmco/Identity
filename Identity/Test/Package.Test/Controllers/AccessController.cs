63da84841e300ab8bae5a13bc67d67a8ec6a94a6
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-05-18 23:39:38 -0700

    create new project for test package

---
+++ Identity/Test/Package.Test/Controllers/AccessController.cs
@@ -0,0 +1,25 @@
+﻿using Identity.Authentication.Extension;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Package.Test.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AccessController : ControllerBase
+{
+    readonly IConfiguration _configuration;
+
+    public AccessController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    //[Auth(PageName = "Owner")]
+    [HttpGet("CheckAccess")]
+    public async Task<IActionResult> CheckAccess()
+    {
+        return await HttpContext.CheckAccessAsync("AdminHome", _configuration)
+            ? Ok(new { Access = true }) : Ok(new { Access = false });
+    }
+}
---
+++ Identity/Test/Test/ServiceInjection.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Servant.Service.Implemention.Injector;
+using System;
+using System.IO;
+
+namespace Test.Injection;
+
+public class ServiceInjection
+{
+    readonly IServiceCollection _serviceCollection;
+
+    readonly IServiceProvider _serviceProvider;
+
+    readonly IConfigurationRoot _configuration;
+
+    public ServiceInjection()
+    {
+        _serviceCollection = new ServiceCollection();
+        _serviceProvider = _serviceCollection.BuildServiceProvider();
+        _configuration = CreateConfiguration();
+        InjectServices();
+    }
+
+    public T? GetService<T>(Type serviceType)
+    {
+        var service = _serviceProvider.GetService(serviceType);
+        if (service != null)
+            return (T)service;
+
+        return default(T);
+    }
+
+    public async void InjectServices()
+    {
+        await _serviceCollection.AddIdentityServicesAsync(_configuration);
+    }
+
+    public IConfigurationRoot CreateConfiguration()
+    {
+        var dir = Directory.GetCurrentDirectory();
+        var settingPath = Directory.GetCurrentDirectory() + "/appsettings.json";
+
+        if (!File.Exists(settingPath))
+        {
+            FileStream fs = new(settingPath, FileMode.OpenOrCreate);
+            fs.Write(new byte[] { });
+            fs.Close();
+            fs.Dispose();
+        }
+
+        var config = new ConfigurationBuilder().SetBasePath(dir)
+                .AddJsonFile(settingPath).Build();
+        return config;
+    }
+}
