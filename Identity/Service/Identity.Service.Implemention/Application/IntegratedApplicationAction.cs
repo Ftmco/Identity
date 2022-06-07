8d58f1cffb7daca6156561fcbc4d7e745ac383cc
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-06-07 01:18:01 -0700

    create integrated app services

---
+++ Identity/Service/Identity.Service.Implemention/Application/IntegratedApplicationAction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Service.Implemention;
+
+public class IntegratedApplicationAction : IIntegratedApplicationAction
+{
+    readonly IBaseCud<IntegeratedApplication,ApplicationContext>
+
+    public ValueTask DisposeAsync()
+    {
+        GC.SuppressFinalize(this);
+        return ValueTask.CompletedTask;
+    }
+}
