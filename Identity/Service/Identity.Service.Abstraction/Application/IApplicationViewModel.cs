bb6607ca722825e1b19bd92258c6558424b9de89
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-06-07 02:53:54 -0700

    create application view model services

---
+++ Identity/Service/Identity.Service.Abstraction/Application/IApplicationViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Service.Abstraction;
+
+public interface IApplicationViewModel : IAsyncDisposable
+{
+    Task<ApplicationInfo> CreateAppInfoViewModelAsync(Application application);
+
+    Task<IEnumerable<ApplicationInfo>> CreateAppInfoViewModelAsync(IEnumerable<Application> applications);
+}
---
+++ Identity/Service/Identity.Service.Implemention/Application/ApplicationViewModelService.cs
@@ -0,0 +1,35 @@
+﻿using Identity.DataBase.ViewModel;
+using Identity.Service.Tooling.Date;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identity.Service.Implemention;
+
+public class ApplicationViewModelService : IApplicationViewModel
+{
+    public Task<ApplicationInfo> CreateAppInfoViewModelAsync(Application application)
+    {
+        ApplicationInfo appInfo = new(Id: application.Id,
+            ParentId: application.ApplicationId,
+            Name: application.Name,
+            ApiKey: application.ApiKey,
+            Key: application.Key,
+            CreateDate: application.CreateDate.ToShamsi(),
+            IsActive: application.IsActive);
+        return Task.FromResult(appInfo);
+    }
+
+    public Task<IEnumerable<ApplicationInfo>> CreateAppInfoViewModelAsync(IEnumerable<Application> applications)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask DisposeAsync()
+    {
+        GC.SuppressFinalize(this);
+        return ValueTask.CompletedTask;
+    }
+}
